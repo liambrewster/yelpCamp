3cea4f79737dc75ab149cf6712ac9ec26016fdce
Liam <liam.brewster@hotmail.co.uk>
2021-07-11 10:19:31 +0100

    feat(flash message) adding and setting up flash message
    added the flash message as per videohttps://www.udemy.com/course/the-web-developer-bootcamp/learn/lecture/22291778#questions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
@@ -46,6 +47,13 @@ const sessionConfig = {
     }
 }
 app.use(session(sessionConfig))
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
 
 // info to render views for the pages
 app.engine('ejs', ejsMAte)
