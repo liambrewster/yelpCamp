8b9c5c81e729b5860262ad54bf18d76fd1c2b3e3
Liam <liam.brewster@hotmail.co.uk>
2021-07-19 23:15:21 +0100

    feat(authorisation) adding permission onto the reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,13 +4,14 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const { validateReview } = require('../middleware.js');
+const { validateReview, isLoggedIn } = require('../middleware.js');
 
 
 //Route for submitting the campground reviews
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
