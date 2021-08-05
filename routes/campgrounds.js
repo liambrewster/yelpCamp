0df48eab4a750b8c6236b613a42f6e5618ec43de
Liam <liam.brewster@hotmail.co.uk>
2021-08-05 21:19:34 +0100

    update(campground) forgot to remove route from the new format causing an error

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,9 +14,9 @@ router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 // Details page for an individual campground
 router.route('/:id')
-    .get('/:id', catchAsync(campgrounds.showCampground))
-    .put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-    .delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 //This route will be used to show campground informaiton for editing
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
