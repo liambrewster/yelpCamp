2185d98603483582f54164f9bf8304ad2c55c46a
Liam <liam.brewster@hotmail.co.uk>
2021-08-05 21:21:50 +0100

    refactor(users) to follow the new reduced format for routes

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,13 @@ const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 router.get('/logout', users.logout);
 
