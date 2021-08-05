5283855a6355e56e566ecd8cca0dc534a7dbe983
Liam <liam.brewster@hotmail.co.uk>
2021-08-05 21:23:05 +0100

    update(users) fixed a typo

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.route('/register')
     .get(users.renderRegister)
     .post(catchAsync(users.register));
 
-router.route('login')
+router.route('/login')
     .get(users.renderLogin)
     .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
