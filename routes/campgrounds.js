7688252edc51dce8959742943fdf802d3a3e0c7c
Liam <liam.brewster@hotmail.co.uk>
2021-08-05 21:14:03 +0100

    refactor(campground) refactor the routes by condesnising based on same route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,27 +5,20 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware.js');
 
-
-
-// Listing all the Campgrounds
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 // Create a new campground
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-// receive the new campground request, save it  and take to show page
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
 // Details page for an individual campground
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+    .get('/:id', catchAsync(campgrounds.showCampground))
+    .put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 //This route will be used to show campground informaiton for editing
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-//this is the route for the edit form to push the update to the server
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-// Delete page for an individual campground
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
