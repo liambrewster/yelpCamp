a78afe668e9fed94730fb2a65a7b54e79225b5c4
Liam <liam.brewster@hotmail.co.uk>
2021-08-21 21:38:19 +0100

    update(image) change edit form to add dunction to upload more images

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,7 +18,7 @@ router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 // Details page for an individual campground
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 //This route will be used to show campground informaiton for editing
