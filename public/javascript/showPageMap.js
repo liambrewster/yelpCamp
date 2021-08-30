4003eee1e5b0057cdc7fcc31e4733dbeadb53d5a
Liam Brewster <liam.brewster@hotmail.co.uk>
2021-08-31 00:06:39 +0100

    feat(maps) cahnge for map to use the campgounds marker

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-1.470832, 53.370351], // starting position [lng, lat]
+    center: campgrounds.geometry.coordinates, // starting position [lng, lat]
     zoom: 13 // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat(campgrounds.geometry.coordinates)
+    .addTo(map)
