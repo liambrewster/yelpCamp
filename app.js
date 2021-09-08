05a4fdcefb0e1a54e0283ad231f31566b78a9d12
Liam Brewster <liam.brewster@hotmail.co.uk>
2021-09-08 21:01:18 +0100

    security: added santise to stop injection attacks

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const morgan = require('morgan');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 //Routes
 const campgroundRoutes = require('./routes/campgrounds');
@@ -41,6 +42,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
@@ -64,6 +66,7 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req, res, next) => {
+console.log(req.query)
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     res.locals.currentUser = req.user;
