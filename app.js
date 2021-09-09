31e420d897a3239e6716ab0e5f341613c5bec92c
Liam Brewster <liam.brewster@hotmail.co.uk>
2021-09-09 22:31:47 +0100

    bug: uninstall mongo-connect and downgrade to remove error

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const userRoutes = require('./routes/users');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
